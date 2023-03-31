aa85ca43d38bafa650faae772b82f56daf22b5d8
LuisGabrielMascarenhas <luis.santos614@etec.sp.gov.br>
2023-03-30 22:00:49 -0300

    Adicionando o Switch

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,8 +1,10 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,13 +21,40 @@ public class MainActivity extends AppCompatActivity {
 
         //Apresentando o xml para o java
         idToolBar = findViewById(R.id.idToolBar);
-
+        //Criando Clique no menu de navegação
         idToolBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Cliquei no menu hamburger",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Cliquei no menu hamburger", Toast.LENGTH_SHORT).show();
             }
         });
+        //Criando clique nos itens de menu
+        idToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
 
+                switch (item.getItemId()) {
+                    case R.id.mFavorito:
+                        Toast.makeText(getApplicationContext(), "Cliquei no  Favorito", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mBuscar:
+                        Toast.makeText(getApplicationContext(), "Cliquei no Buscar", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mCompartilhar:
+                        Toast.makeText(getApplicationContext(), "Cliquei no Compartilhar", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mSalvar:
+                        Toast.makeText(getApplicationContext(),"Cliquei no Salvar",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mSair:
+                        Toast.makeText(getApplicationContext(), "Cliquei no Sair", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+
+                return false;
+            }
+        });
     }
 }
